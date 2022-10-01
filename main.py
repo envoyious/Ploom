2ce6c3556a99c0291a485f2230cfe3c8898f08b4
abyssalEnvoy <zain.akram05@outlook.com>
2022-10-01 22:53:49 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+# DoomEngine, 0.00 WIP
+# Developed by Zain Akram
+
+import sys
+import pygame
+
+# Screen size
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 360
+VIEW_WIDTH = 320
+VIEW_HEIGHT = 180
+
+CLEAR_COLOR = (pygame.Color("black"))
+
+FRAMERATE = 60
+
+def main():
+    # Inintialise pygame
+    pygame.init()
+    pygame.display.set_caption("DoomEngine")
+    clock = pygame.time.Clock()
+
+    # Set display mode
+    flags = pygame.DOUBLEBUF | pygame.RESIZABLE
+    graphics = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), flags)
+    graphics.set_alpha(None)
+    target = pygame.Surface((VIEW_WIDTH, VIEW_HEIGHT))
+
+    # Main loop
+    is_running = True
+    while is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+    
+        # Rendering
+        graphics.fill(pygame.Color("black"))        
+        target.fill(CLEAR_COLOR)
+        pygame.draw.line(target, pygame.Color("white"), (0, 0), (16, 16))
+
+        #region Render target resizing
+
+        window_width, window_height = graphics.get_size()
+        scale = min(window_width / VIEW_WIDTH, window_height / VIEW_HEIGHT)
+
+        bar_width = int((window_width - int(VIEW_WIDTH * scale)) / 2)
+        bar_height = int((window_height - int(VIEW_HEIGHT * scale)) / 2)
+
+        resized = pygame.transform.scale(target, (int(VIEW_WIDTH * scale), int(VIEW_HEIGHT * scale)))
+        graphics.blit(resized, (bar_width, bar_height))
+
+        #endregion
+
+        pygame.display.update()
+        
+        # Cap the framerate at 60 frames per second
+        clock.tick(FRAMERATE)
+        pygame.display.set_caption("DoomEngine, FPS: {}".format(clock.get_fps()))
+
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
