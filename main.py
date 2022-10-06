7c543585c6d0afabfe7146d05ea3511bdcc74cd4
abyssalEnvoy <zain.akram05@outlook.com>
2022-10-06 21:37:22 +0100

    Added clipping

--- main.py
+++ main.py
@@ -62,6 +62,17 @@ class Wall:
         self.x2 = x2
         self.y2 = y2
 
+def two_by_two_determinant(x1, y1, x2, y2):
+    return x1 * y2 - y1 * x2
+
+def intersect(x1, y1, x2, y2, x3, y3, x4, y4):
+    x = two_by_two_determinant(x1, y1, x2, y2)
+    y = two_by_two_determinant(x3, y3, x4, y4)
+    det = two_by_two_determinant(x1 - x2, y1 - y2, x3 - x4, y3 - y4)
+    x = two_by_two_determinant(x, x1 - x2, y, x3 - x4) / det
+    y = two_by_two_determinant(x, y1 - y2, y, y3 - y4) / det
+    return x, y
+
 def main():
     # Inintialise pygame
     pygame.init()
@@ -79,7 +90,7 @@ def main():
     world_view = pygame.Surface((98, 109))
 
     # Initialise objects
-    player = Player(50, 50, 0)
+    player = Player(49, 49, 0)
     wall = Wall(70, 20, 70, 70)
 
     # Main loop
@@ -146,20 +157,46 @@ def main():
         
         #region World view
 
-        # Transforming 3D coordinate onto the 2D plane
+        # Clip the walls
+                
+        if ry1 > 0 or ry2 > 0:
+            nearz = 0.0001
+            farz = 5
+            nearside = 0.0001
+            farside = 1000
 
-        x1 = -rx1 * 16 / ry1
-        y1a = -50 / ry1 
-        y1b = 50 / ry1
+            ix1, iy1 = intersect(rx1, ry1, rx2, ry2, -nearside, nearside, -farside, farz)
+            ix2, iy2 = intersect(rx1, ry1, rx2, ry2, nearside, nearside, farside, farz)
 
-        x2 = -rx2 * 16 / ry2
-        y2a = -50 / ry2
-        y2b = 50 / ry2
+            if ry1 <= 0:
+                if iy1 > 0:
+                    rx1 = ix1
+                    ry1 = iy1
+                else:
+                    rx1 = ix2
+                    ry1 = iy2
+            
+            if ry2 <= 0:
+                if iy1 > 0:
+                    rx2 = ix1
+                    ry2 = iy1
+                else:
+                    rx2 = ix2
+                    ry2 = iy2
+        
+            # Transforming 3D coordinate onto the 2D plane
+            x1 = -rx1 * 16 / ry1
+            y1a = -49 / ry1 
+            y1b = 49 / ry1
 
-        pygame.draw.line(world_view, pygame.Color("yellow"), (50 + x1, 50 + y1a), (50 + x2, 50 + y2a)) # Top line
-        pygame.draw.line(world_view, pygame.Color("yellow"), (50 + x1, 50 + y1b), (50 + x2, 50 + y2b)) # Bottom line
-        pygame.draw.line(world_view, pygame.Color("red"), (50 + x1, 50 + y1a), (50 + x1, 50 + y1b)) # Left
-        pygame.draw.line(world_view, pygame.Color("red"), (50 + x2, 50 + y2a), (50 + x2, 50 + y2b)) # Right
+            x2 = -rx2 * 16 / ry2
+            y2a = -49 / ry2
+            y2b = 49 / ry2
+
+            pygame.draw.line(world_view, pygame.Color("yellow"), (49 + x1, 49 + y1a), (49 + x2, 49 + y2a)) # Top line
+            pygame.draw.line(world_view, pygame.Color("yellow"), (49 + x1, 49 + y1b), (49 + x2, 49 + y2b)) # Bottom line
+            pygame.draw.line(world_view, pygame.Color("red"), (49 + x1, 49 + y1a), (49 + x1, 49 + y1b)) # Left
+            pygame.draw.line(world_view, pygame.Color("red"), (49 + x2, 49 + y2a), (49 + x2, 49 + y2b)) # Right
 
         pygame.draw.rect(world_view, pygame.Color("cyan"), pygame.Rect(0, 0, 98, 109), 1)
 
